0c76e23be980cb6055c6faa1fb635c73379cf644
dhruv-yadav-49 <dhruvofficial73@gmail.com>
2025-12-31 07:34:41 +0530

    Fix MongoDB

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(cors({
 }));
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
